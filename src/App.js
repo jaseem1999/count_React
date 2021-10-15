707606355dc8b8983b0dfcefed80f9a5b43f2556
jaseem <65609243+jaseem1999@users.noreply.github.com>
2021-10-15 15:20:13 +0530

    first commit react app form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react'
+import Counter from './Counter'
 
 function App() {
+  const [count,setCount] = useState(0)
+  const addCount=()=> {
+      setCount(count+1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="App">
+       <button onClick={addCount}>add</button>
+       <Counter title="1st counter" count = {count}/>
+       <Counter title="2nd counter" count = {count}/>
+     </div>
+    
   );
 }
 
